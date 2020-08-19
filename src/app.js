36a80b08780db91cf0bd0f2b5e821582d6683e71
Ruly Anggriawan <ruly.anggriawan@live.com>
2020-08-19 22:49:02 +0700

    simple fetch

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
 import "../styles/style.css";
 import "./skewed-box.js"
 
-const divElement = document.createElement("div");
+const searchButtonElement = document.querySelector("#search-button-element");
+const searchElement = document.querySelector("#search-element");
+const mealList = document.querySelector("#meal-list");
 
+const onButtonSearchClicked = () => {
+    console.log(searchElement.value);
+    getMeal(searchElement.value);
+}
 
-const headingElement = document.createElement("h1");
-headingElement.innerText = "ini kontent dalam shadow DOM";
+searchButtonElement.addEventListener("click", onButtonSearchClicked);
 
-const shadowRoot = divElement.attachShadow({mode:"open"});
+const getMeal = async (name) => {
+    try{
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+        const responseJson = await response.json();
+        // console.log(responseJson.meals)
+        render(responseJson.meals);
+    }catch(error){
+        console.log(error);
+    }
+}
 
-shadowRoot.appendChild(headingElement);
-
-document.body.appendChild(divElement);
-
-console.log("test125");
+const render = (meals) => {
+    mealList.innerHTML = '';
+    const ul = document.createElement("ul");
+    meals.forEach( meal => {
+        const li = document.createElement("li");
+        li.innerHTML = `${meal.strMeal}`;
+        ul.appendChild(li);
+        console.log(meal);
+    });
+    mealList.appendChild(ul);
+}
